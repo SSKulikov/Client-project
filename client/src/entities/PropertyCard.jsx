43c2a079ad39980b1a1d12bd998f0f4cc380b331
MaratIl <117185885+MaratIl@users.noreply.github.com>
2025-11-27 11:09:41 +0300

    Grid cards done

--- client/src/entities/PropertyCard.jsx
+++ client/src/entities/PropertyCard.jsx
@@ -2,22 +2,20 @@ import { Card, ListGroup } from "react-bootstrap";
 
 function PropertyCard({ property }) {
   return (
-    <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-        <Card.Body>
-          <Card.Title>{property.type}</Card.Title>
-          <Card.Text>{property.descriptions}</Card.Text>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item> Цена: {property.price} руб</ListGroup.Item>
-          <ListGroup.Item>Адрес: {property.addres}</ListGroup.Item>
-        </ListGroup>
-        <Card.Body>
-          <Card.Link href="#">Написать</Card.Link>
-        </Card.Body>
-      </Card>
-    </>
+    <Card style={{ width: "100%", height: "100%" }}>
+      <Card.Img variant="top" src={property.image} />
+      <Card.Body>
+        <Card.Title>{property.type}</Card.Title>
+        <Card.Text>{property.descriptions}</Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item> Цена: {property.price} руб</ListGroup.Item>
+        <ListGroup.Item>Адрес: {property.addres}</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+        <Card.Link href="#">Написать</Card.Link>
+      </Card.Body>
+    </Card>
   );
 }
 
